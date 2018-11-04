c7fc1c35b75fb5152911f2c72fa30c71434858c0
Andrii Meretskyi <andre719mv81@gmail.com>
2018-11-04 11:00:45 +0200

    use snapshots

--- QueryOrm/sample/src/main/java/com/stayfit/sample/MyApplication.java
+++ QueryOrm/sample/src/main/java/com/stayfit/sample/MyApplication.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import com.stayfit.queryorm.lib.DOBase;
 import com.stayfit.queryorm.lib.DbHelper;
 import com.stayfit.queryorm.lib.QueryParms;
+import com.stayfit.queryorm.lib.SqlExecutor;
 import com.stayfit.queryorm.lib.sqlinterfaces.ISQLiteDatabase;
 import com.stayfit.sample.dal.AppDb;
 import com.stayfit.sample.dal.DbCoumns;
@@ -77,5 +78,10 @@ public class MyApplication extends Application {
         };
 
         thread.start();
+
+        SqlExecutor executor = new SqlExecutor();
+        SmartSqlQuery query = new SmartSqlQuery();
+        executor.executeSelect(PersonEntity.class, query);
+
     }
 }
